d39b8616e5cc74a549259a25c94008d25cf74908
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-31 20:10:13 +0300

    Design homepage header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import VideoCard from "../components/VideoCard";
 import ArticleCard from "../components/ArticleCard";
+import Header from "../components/Header";
 
 const HomePage = () => {
   const dummyData = [1, 2, 3, 4, 5, 6];
   return (
     <>
+      <Header />
       <VideoCard />
       {dummyData.map((card) => {
         return <ArticleCard key={card} />;
