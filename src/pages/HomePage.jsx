ad483b378d40d20b2c3179e9c0e5986a3b18350b
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-31 21:30:20 +0300

    Add section to display new posts titles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import VideoCard from "../components/VideoCard";
 import ArticleCard from "../components/ArticleCard";
 import Header from "../components/Header";
 import FeaturedView from "../components/FeaturedView";
+import NewPostSection from "../components/NewPostSection";
 
 const HomePage = () => {
   return (
     <>
       <Header />
       <FeaturedView />
+      <NewPostSection />
     </>
   );
 };
