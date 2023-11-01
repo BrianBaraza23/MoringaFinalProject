1609e5c082db97c64de0a19bc7be634d692d2252
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-02 02:35:08 +0300

    Add user lists section

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../components/dashboard/Sidebar";
 import TopBar from "../components/dashboard/TopBar";
 import ProfileSection from "../components/dashboard/ProfileSection";
 import CategorySection from "../components/dashboard/CategorySection";
+import UsersList from "../components/dashboard/UsersList";
 
 const Dashboard = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -16,6 +17,7 @@ const Dashboard = () => {
             <div className='px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto'>
               <ProfileSection />
               <CategorySection />
+              <UsersList />
             </div>
           </main>
         </div>
