c6ca25189b831df02ba8c2142c1ed6e6f9beaa44
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-02 00:57:29 +0300

    Add sidebar content

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -7,8 +7,22 @@ const Sidebar = ({ isCollapsed }) => {
         isCollapsed ? "w-16" : "w-48"
       } z-40 md:z-0 h-screen overflow-y-scroll scrollbar-hide left-0 top-0 transition-all duration-300 ease-in-out`}
     >
-      <div className='sticky top-0 bg-gray-700 flex gap-4'></div>
-      <div className='px-4 pb-4 mb-8'></div>
+      <div className='flex flex-col items-center'>
+        {isCollapsed ? (
+          <i
+            className='fa fa-user-circle text-3xl text-white mt-4 mx-auto'
+            aria-hidden='true'
+          ></i>
+        ) : (
+          <img
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_os0byb3xeANO3FvA5MgMYvIbmMpxI7VOUg&usqp=CAU'
+            alt='user'
+            className={`w-16 h-16 rounded-full mt-8`}
+          />
+        )}
+        <h6 className='text-default-gold py-1'>Yehezkiel Bryan</h6>
+        <h6 className='text-white font-semibold italic'>Administrator</h6>
+      </div>
     </aside>
   );
 };
