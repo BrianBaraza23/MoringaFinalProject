c53d924565d2ef806166f1c600606c7c4beef111
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-03 23:15:15 +0300

    Add responsive design to header and featured article view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,27 +6,33 @@ import HttpsIcon from "@mui/icons-material/Https";
 
 const Header = () => {
   return (
-    <div className='bg-default-gold flex justify-between py-2 md:px-12 border-bottom'>
-      <div className='flex gap-2 items-center'>
-        <h2 className='text-default-green font-bold text-2xl my-auto'>MSD</h2>
+    <div className='bg-default-gold flex flex-wrap items-center md:justify-between py-2 px-4 md:px-12 border-bottom'>
+      <div className='w-full md:w-auto flex gap-2 items-center justify-between md:justify-start'>
+        <div className='flex items-center gap-1'>
+          <i
+            className='fa fa-bullhorn text-default-green text-3xl md:text-4xl'
+            aria-hidden='true'
+          ></i>
+          <h2 className='text-default-green font-bold text-4xl my-auto'>MSD</h2>
+        </div>
         <h6 className='text-white my-auto font-semibold'>
           Keeping you a click ahead
         </h6>
       </div>
-      <ul className='text-default-green flex gap-3'>
+      <ul className='text-default-green w-full md:w-auto flex justify-between items-center md:gap-3 my-auto list-none py-2 md:py-0'>
         <li>
           <Link to='/dashboard' className='flex gap-1 items-center'>
             <PersonIcon style={{ fontSize: "22px" }} />
-            <h6 className='my-auto text-sm font-semibold'>Dashboard</h6>
+            <h6 className='my-auto text-md font-semibold'>Dashboard</h6>
           </Link>
         </li>
         <li className='flex gap-1 items-center'>
           <StarIcon style={{ fontSize: "22px" }} />
-          <h6 className='my-auto text-sm font-semibold'>Wishlist</h6>
+          <h6 className='my-auto text-md font-semibold'>Wishlist</h6>
         </li>
         <li className='flex gap-1 items-center'>
           <HttpsIcon style={{ fontSize: "22px" }} />
-          <h6 className='my-auto text-sm font-semibold'>Logout</h6>
+          <h6 className='my-auto text-md font-semibold'>Logout</h6>
         </li>
       </ul>
     </div>
