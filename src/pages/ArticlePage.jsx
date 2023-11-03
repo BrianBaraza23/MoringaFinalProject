8a6a55ba8baf76038a94cd5ef7a183520fb83716
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-03 20:37:56 +0300

    Add footer

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Article from "../components/Article";
 import ArticlesSection from "../components/ArticlesSection";
+import Footer from "../components/Footer";
 
 const ArticlePage = () => {
   return (
@@ -9,6 +10,7 @@ const ArticlePage = () => {
       <Header />
       <Article />
       <ArticlesSection section_color='bg-default-green' />
+      <Footer />
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import NewPostSection from "../components/NewPostSection";
 import TrendingMedia from "../components/TrendingMedia";
 import VideoSlider from "../components/VideoSlider";
 import ArticlesSection from "../components/ArticlesSection";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
@@ -17,6 +18,7 @@ const HomePage = () => {
       <TrendingMedia />
       <VideoSlider />
       <ArticlesSection section_color='bg-white' />
+      <Footer />
     </>
   );
 };
