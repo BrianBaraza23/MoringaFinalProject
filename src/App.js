e5bdcd4fd136f157afc34cab8d5c2318bb4943a1
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-07 23:41:10 +0300

    Integrate articles endpoints & render to screen

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import ArticlePage from "./pages/ArticlePage";
 import Dashboard from "./pages/Dashboard";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { listArticles } from "./redux/actions/articleActions";
 
 function App() {
+  const dispatch = useDispatch();
+  const article = useSelector((state) => state.article);
+  const { articles, success_create } = article;
+
+  useEffect(() => {
+    dispatch(listArticles());
+  }, [dispatch, success_create]);
+
+  console.log(articles);
   return (
     <Router>
       <Routes>
